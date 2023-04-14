0f57dceca9e7d8647ccce819bd529ab1a05f3642
sam0verfl0w <stales@student.42angouleme.fr>
2023-04-14 15:13:47 +0200

    update

--- tests/main.c
+++ tests/main.c
@@ -7,6 +7,9 @@ SUITE(node_tests)
 	RUN_TEST(node_new_void);
 	RUN_TEST(node_free_test1);
 	RUN_TEST(node_test_push);
+	RUN_TEST(node_test_push_back);
+	RUN_TEST(node_test_push_back2);
+	RUN_TEST(node_test_push2);
 }
 
 int main(int argc, char **argv)
--- tests/node_test.h
+++ tests/node_test.h
@@ -50,7 +50,7 @@ TEST	node_free_test1(void)
 
 TEST	node_test_push(void)
 {
-	node_t	*ptr[0x5];
+	node_t	*ptr[0x10];
 	node_t	*lst = NULL;
 	size_t	i = 0;
 
@@ -71,4 +71,111 @@ TEST	node_test_push(void)
 	PASS();
 }
 
+TEST	node_test_push_back(void)
+{
+	node_t	*ptr[0x10];
+	node_t	*lst = NULL;
+	size_t i = 0;
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		ptr[i] = node_new((int *)i);
+		ASSERT_NEQ(ptr[i], NULL);
+		ASSERT_EQ(ptr[i]->content, (int *)i);
+	}
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		node_push_back(&lst, ptr[i]);
+		ASSERT_EQ(node_get_last(lst), ptr[i]);
+	}
+
+	node_free_all(&lst);
+	PASS();
+}
+
+TEST	node_test_push_back2(void)
+{
+	node_t	*ptr[0x10];
+	node_t	*lst = NULL;
+	node_t	*tmp = NULL;
+	size_t i = 0;
+	size_t size = 0;
+	index_t	idx = 0;
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		ptr[i] = node_new((int *)i);
+		ASSERT_NEQ(ptr[i], NULL);
+		ASSERT_EQ(ptr[i]->content, (int *)i);
+	}
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		node_push_back(&lst, ptr[i]);
+		ASSERT_EQ(node_get_last(lst), ptr[i]);
+	}
+
+	tmp = lst;
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		ASSERT_EQ(tmp, ptr[i]);
+		ASSERT_EQ(tmp->prev, ptr[i]->prev);
+		ASSERT_EQ(tmp->next, ptr[i]->next);
+		ASSERT_EQ(tmp->content, ptr[i]->content);
+		tmp = tmp->next;
+	}
+
+	tmp = node_get_last(lst);
+	size = node_get_size(lst);
+	idx = i - 1; 
+
+	while (idx >= 0 && idx <= size) {
+		ASSERT_EQ(tmp, ptr[idx]);
+		ASSERT_EQ(tmp->content, ptr[idx]->content);
+		ASSERT_EQ(tmp->prev, ptr[idx]->prev);
+		ASSERT_EQ(tmp->next, ptr[idx]->next);
+		tmp = tmp->prev;
+		idx--;
+	}
+
+	node_free_all(&lst);
+	PASS();
+}
+
+TEST	node_test_push2(void)
+{
+	node_t	*ptr[0x10];
+	node_t	*lst = NULL;
+	node_t	*tmp = NULL;
+	size_t i = 0;
+	size_t size = 0;
+	index_t	idx = 0;
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		ptr[i] = node_new((int *)i);
+		ASSERT_NEQ(ptr[i], NULL);
+		ASSERT_EQ(ptr[i]->content, (int *)i);
+	}
+
+	for (i = 0; i < sizeof(ptr)/sizeof(node_t*); i++) {
+		node_push(&lst, ptr[i]);
+		ASSERT_EQ(lst, ptr[i]);
+		ASSERT_EQ(lst->content, ptr[i]->content);
+		ASSERT_EQ(lst->next, ptr[i]->next);
+		ASSERT_EQ(lst->prev, ptr[i]->prev);
+	}
+
+	tmp = node_get_last(lst);
+	size = node_get_size(lst);
+	idx = i - 1; 
+
+	while (idx >= 0 && idx <= size) {
+		ASSERT_EQ(tmp, ptr[idx]);
+		ASSERT_EQ(tmp->content, ptr[idx]->content);
+		ASSERT_EQ(tmp->prev, ptr[idx]->prev);
+		ASSERT_EQ(tmp->next, ptr[idx]->next);
+		tmp = tmp->next;
+		idx--;
+	}
+
+	node_free_all(&lst);
+	PASS();
+}
 #endif
