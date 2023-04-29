11dac8608a31f52ab527a5529cd98abf77eac38e
sam0verfl0w <stales@student.42angouleme.fr>
2023-04-29 21:13:05 +0200

    update

--- inc/tlist.h
+++ inc/tlist.h
@@ -4,18 +4,36 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-#define TLIST(T,L)				\
-	struct _list##L { 			\
-		struct _list##L	*_next;	\
-		struct _list##L	*_prev;	\
-		T				_data;	\
+#define TLIST(T,L)										\
+	struct _list##L { 									\
+		struct _list##L	*_next;							\
+		struct _list##L	*_prev;							\
+		T				_data;							\
 	} L = { NULL, NULL };
 
-#define TLIST_TYPE(T, L)				\
-	typedef struct _list##L {			\
-		struct _list##L	*_next;			\
-		struct _list##L	*_prev;			\
-		T				_data;			\
+#define TLIST_TYPE(T, L)								\
+	typedef struct _list##L {							\
+		struct _list##L	*_next;							\
+		struct _list##L	*_prev;							\
+		T				_data;							\
 	} L;
+
+#define TLIST_T(T, L, D)\
+	typeof(T)	L = D;
+
+#define TLIST_PTR(T, L, D)\
+	typeof(T)	*L = D;
+
+#define TLIST_NEW_STACK(T, L)							\
+	 typeof(T)	L;\
+
+#define TLIST_NEW(T, L)									\
+	({\
+	 typeof(T)	*L = typeof(T)malloc(sizeof(T));		\
+	 })
+
+#define TLIST_FREE(ptr)\
+	free(ptr);\
+	ptr = NULL;
 
 #endif
